5a22a3b188f29c4a49194a87e3534d1ae82a2edb
Ivan <ivan@insaite.io>
2018-12-19 13:04:03 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,6 +19,11 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
 	urlDB = 'mongodb://localhost:27017/cafe'; //link de base de datos remota
 }else{
-	urlDB = 'mongodb://cafe-user:root123456!@ds015849.mlab.com:15849/cafe';  //link de base de datos remota pass: root123456!
+	urlDB = process.env.MONGO_URI; /*'mongodb://cafe-user1:root123456@ds015849.mlab.com:15849/cafe'se enmascara*/  //link de base de datos remota pass: root123456!
 }
 process.env.URLDB= urlDB;
+
+//comandos
+//heroku config:set <nombreDeVariable>=<asignación_del_url_enmascarado> <-- setar una variable de entorno en heroku
+//heroku config:unset <nombreDeVariable> <-- para borrar la variable de entorno de heroku
+//heroku config <--  ver las variables de entorno corriendo sobre heroku
